0f1ecdee8dd920662b7f6e9c7b313d3ef8a90947
Macarena Ruiz <contactomruiz@yahoo.com>
2022-11-04 01:23:42 -0300

    lyrics script

---
+++ Maqui'sPlaylist/Assets/Scripts/Lyrics.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Lyrics : MonoBehaviour
+{
+    public List<Text> lyricText;
+    public List<float> timeStamp;
+    void Start()
+    {
+        
+    }
+
+    public void LyricsTimeStamp()
+    {
+        
+    }
+}
